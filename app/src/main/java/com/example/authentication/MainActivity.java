96b3bb72e6f1828a88016ff5e05646e21caac366
BurhanDundar <57062662+BurhanDundar@users.noreply.github.com>
2021-12-22 18:27:09 +0300

    ortaya karisik islemler...

--- app/src/main/java/com/example/authentication/MainActivity.java
+++ app/src/main/java/com/example/authentication/MainActivity.java
@@ -8,6 +8,7 @@ package com.example.authentication;
         import android.os.Bundle;
         import android.util.Log;
         import android.view.View;
+        import android.view.WindowManager;
         import android.widget.Button;
         import android.widget.TextView;
         import android.widget.Toast;
@@ -18,71 +19,12 @@ package com.example.authentication;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView welcome;
-    String userName = "";
-    String splitEmail[];
-    Button btnLogOut;
-    FirebaseAuth mAuth;
-    String header;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnLogOut = findViewById(R.id.btnLogout);
-        mAuth = FirebaseAuth.getInstance();
-        welcome = (TextView) findViewById(R.id.textView);
-
-        btnLogOut.setOnClickListener(view->{
-            mAuth.signOut();
-            startActivity(new Intent(MainActivity.this,LoginActivity.class));
-        });
-    }
-    @Override
-    protected void onStart(){
-        super.onStart();
-        FirebaseUser user = mAuth.getCurrentUser();
-        if(user != null) {
-            splitEmail = user.getEmail().split("@",0); // welcome mesajını maile göre dinamik yazdırma kısmı
-            userName = splitEmail[0];
-            welcome.setText("Welcome " + userName + " :)");
-        }
-        else{
-            startActivity(new Intent(MainActivity.this,LoginActivity.class));
-        }
-    }
-
-
-    public void cardActivity(@NonNull View v){
-
-        TextView name = (TextView) findViewById(v.getId()) ;
-        Intent myIntent = new Intent(MainActivity.this,CardDetails.class);
-        myIntent.putExtra("name",name.getText().toString());
-        startActivity(myIntent);
-
-
-       /* else if(v.getId() == R.id.art){
-            Intent myIntent = new Intent(MainActivity.this,CardDetails.class);
-            myIntent.putExtra("name",v.getId());
-            startActivity(myIntent);
-        }
-        else if(v.getId() == R.id.food){
-            Intent myIntent = new Intent(MainActivity.this,CardDetails.class);
-            myIntent.putExtra("name",v.getId());
-            startActivity(myIntent);
-        }
-        else if(v.getId() == R.id.game){
-            Intent myIntent = new Intent(MainActivity.this,CardDetails.class);
-            myIntent.putExtra("name",v.getId());
-            startActivity(myIntent);
-        }
-        else if(v.getId() == R.id.chat){
-            Intent myIntent = new Intent(MainActivity.this,CardDetails.class);
-            myIntent.putExtra("name",v.getId());
-            startActivity(myIntent);
-        }*/
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportFragmentManager().beginTransaction().replace(R.id.maincontainer,new DataFragment()).commit();
     }
 }
 
